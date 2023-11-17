427a54a602a6881e35a99bd1dcaafeb652000b6f
alice <alice.caporali@hotmail.fr>
2023-11-17 14:11:37 +0100

    remove dead code and test link in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,36 +14,19 @@ export default function Navbar() {
       name: "Accueil",
       path: "/accueil",
     },
-    {
-      name: "Test",
-      path: "/test",
-    },
   ];
   
-  const callAPI = async () => {
-		try {
-			const res = await fetch(
-				`https://jsonplaceholder.typicode.com/posts/1`
-			);
-			const data = await res.json();
-			console.log(data);
-		} catch (err) {
-			console.log(err);
-		}
-	};
   return (
     <div>
-    <button onClick={callAPI}>Make API Call</button>
-    <nav className='bg-emerald-300 text-white px-8 py-8'>     
-        {navLinks.map((link, index) => {
-          return (    
-            <Link key={index} className={`px-4 py-3 font-medium hover:underline  underline-offset-8 hover:text-slate-900 ${pathname === link.path ? 'underline  underline-offset-8' : ''}`} href={link.path}>
-              {link.name}
-            </Link>
-          );
-        })}
-      
-    </nav>
+      <nav className='bg-emerald-300 text-white px-8 py-8'>     
+          {navLinks.map((link, index) => {
+            return (    
+              <Link key={index} className={`px-4 py-3 font-medium hover:underline  underline-offset-8 hover:text-slate-900 ${pathname === link.path ? 'underline  underline-offset-8' : ''}`} href={link.path}>
+                {link.name}
+              </Link>
+            );
+          })}
+      </nav>
     </div>
   )
 }
