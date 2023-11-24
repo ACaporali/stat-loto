11f631c164a5a3f0cc2e9fdc47ed55c47e19698f
alice <alice.caporali@hotmail.fr>
2023-11-24 18:18:13 +0100

    Update home page text

--- app/accueil/page.tsx
+++ app/accueil/page.tsx
@@ -14,7 +14,7 @@ export default function Accueil() {
       <div className='container mx-auto py-10 text-center'>
         <h1 className='text-xl'>Statistiques relatives aux tirages du Loto Français</h1>
         <p>Application développée avec <Link href="https://nextjs.org/" target="_blank">Next.js 13</Link> et s'appuie sur les données fournies par une API de <Link href="https://data.opendatasoft.com/explore/dataset/resultats-loto-2019-a-aujourd-hui@agrall/" target="_blank">Opendatasoft</Link>.</p>
-        <p>/!\ Le développement de cette application a pour objectif de m'auto-formé sur le framework Next, aucun but commerial./!\</p>
+        <p>/!\ Le développement de cette application a pour objectif de m'auto-former sur le framework Next, aucun but commercial./!\</p>
       </div>
       <div className='container mx-auto py-10'>
         <h1 className='text-xl'>Derniers resultats du loto</h1>    
