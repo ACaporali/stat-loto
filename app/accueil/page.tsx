3de3723f0e5e6f3ed46d8b1fd9333fe331ad33bd
alice <alice.caporali@hotmail.fr>
2023-12-10 21:19:06 +0100

    change table cosmetic

--- app/accueil/page.tsx
+++ app/accueil/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import { use, useEffect } from "react"
 import type { Metadata } from 'next'
@@ -11,12 +13,12 @@ export default function Accueil() {
 
   return (
     <div>
-      <div className='container mx-auto py-10 text-center'>
+      <div className='container mx-auto py-5 text-center'>
         <h1 className='text-xl'>Statistiques relatives aux tirages du Loto Français</h1>
         <p>Application développée avec <Link href="https://nextjs.org/" target="_blank">Next.js 13</Link> et s'appuie sur les données fournies par une API de <Link href="https://data.opendatasoft.com/explore/dataset/resultats-loto-2019-a-aujourd-hui@agrall/" target="_blank">Opendatasoft</Link>.</p>
         <p>/!\ Le développement de cette application a pour objectif de m'auto-former sur le framework Next, aucun but commercial./!\</p>
       </div>
-      <div className='container mx-auto py-10'>
+      <div className='container mx-auto py-5'>
         <h1 className='text-xl'>Derniers resultats du loto</h1>    
         <ShowLotoDraw  />
       </div>
