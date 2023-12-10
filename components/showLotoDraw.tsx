96f7e5c889b466c2214027f478dffd868f12636a
alice <alice.caporali@hotmail.fr>
2023-12-10 21:24:26 +0100

    Fix error _WEBPACK_IMPORTED_MODULE_2___default(...) is not a function

--- components/showLotoDraw.tsx
+++ components/showLotoDraw.tsx
@@ -4,14 +4,11 @@ import Link from 'next/link';
 import use from "react";
 import useSWR from 'swr';
 import { usePathname } from 'next/navigation';
-import {getAPILastDraws, getUserRepos, test} from '../services/loto.service';
+import { getAPILastDraws } from '../services/loto.service';
 
-export default function ShowLotoDraw() {
-  const date = new Date().getFullYear()
+export default function ShowLotoDraw() {  
+  const listDraw = getAPILastDraws(4)
   
-  const listDraw = getAPILastDraws(3)
- 
-
   if (listDraw.isError) return <p>Erreur lors de la recupération des résultats :s </p>;
   if (!listDraw.data) return <p>Chargement des résultats ...</p>;
   
@@ -19,26 +16,26 @@ export default function ShowLotoDraw() {
     <div className='py-2'>
       <table className='table-draw w-full border-collapse rounded-md'>
         <thead>
-          <tr className='bg-slate-300 text-left'>
-            <th className='py-2'>Date</th>
-            <th className='py-2'>Boule 1</th>
-            <th className='py-2'>Boule 2</th>
-            <th className='py-2'>Boule 3</th>
-            <th className='py-2'>Boule 4</th>
-            <th className='py-2'>Boule 5</th>
-            <th className='py-2'>Numéro chance</th>
+          <tr className='bg-gray-200 text-left'>
+            <th>Date</th>
+            <th>Boule 1</th>
+            <th>Boule 2</th>
+            <th>Boule 3</th>
+            <th>Boule 4</th>
+            <th>Boule 5</th>
+            <th>Numéro chance</th>
           </tr>
         </thead>
         <tbody>
           {listDraw.data.results.map(draw => (
-            <tr key={draw.annee_numero_de_tirage} className='border-slate-300'>
-              <td className='py-3'>{draw.date_de_tirage}</td>
-              <td className='py-3'>{draw.boule_1}</td>
-              <td className='py-3'>{draw.boule_2}</td>
-              <td className='py-3'>{draw.boule_3}</td>
-              <td className='py-3'>{draw.boule_4}</td>
-              <td className='py-3'>{draw.boule_5}</td>
-              <td className='py-3'>{draw.numero_chance}</td>
+            <tr key={draw.annee_numero_de_tirage} className='border-gray-200'>
+              <td>{draw.date_de_tirage}</td>
+              <td>{draw.boule_1}</td>
+              <td>{draw.boule_2}</td>
+              <td>{draw.boule_3}</td>
+              <td>{draw.boule_4}</td>
+              <td>{draw.boule_5}</td>
+              <td>{draw.numero_chance}</td>
             </tr>
           ))}
         </tbody>
